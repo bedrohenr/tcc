94018688cde462493c54fd89d7baebaed55ea15f
Pedro Ferreira <ferfonhp@gmail.com>
2026-09-24 17:11:39 -0300

    feat: adicionando dpid aos switches

--- infraestrutura/mininet/topologia.py
+++ infraestrutura/mininet/topologia.py
@@ -13,9 +13,9 @@ class ArvoreTopo(Topo):
 
     def build(self):
         # Switches
-        s1 = self.addSwitch('s1', protocols='OpenFlow13')
-        s2 = self.addSwitch('s2', protocols='OpenFlow13')
-        s3 = self.addSwitch('s3', protocols='OpenFlow13')
+        s1 = self.addSwitch('s1', dpid="s1", protocols='OpenFlow13')
+        s2 = self.addSwitch('s2', dpid="s2", protocols='OpenFlow13')
+        s3 = self.addSwitch('s3', dpid="s3",protocols='OpenFlow13')
 
         # Hosts
         h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
