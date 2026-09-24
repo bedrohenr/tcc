931d5cc47b841b35dd933c022a9e27e6c25d6636
Pedro Ferreira <ferfonhp@gmail.com>
2026-09-24 17:12:52 -0300

    feat: rodando iperf da maquina h1 -> h2
    pra testar uso da velocidade da rede

--- infraestrutura/mininet/__init__.py
+++ infraestrutura/mininet/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+from time import sleep
 from functools import partial
 
 from mininet.net import Mininet
@@ -36,8 +36,25 @@ def startMininet():
     info('[*] Iniciando o Mininet \n')
     net.start()
 
-    info('[*] Pingando todos os hosts \n')
-    net.pingAll()
+    # info('[*] Pingando todos os hosts \n')
+    # net.pingAll()
 
+    # Inicia o teste de comunicacao de todos para todos
+    port = 5001
+    data_size = 5_000_000_000
+    for h in net.hosts:
+        h.cmd('iperf -s -p %s > /dev/null &' % port)
+        
+    h1 = net.get('h1')
+    h2 = net.get('h2')
+    while(True):
+        h1.cmd('iperf -c %s -p %s -n %d -i 1 -yc > /dev/null &' % (h2.IP(), port, data_size))
+        sleep(5)
+
+    # for client in net.hosts:
+    #     for server in net.hosts:
+    #         if client != server:
+    #             client.cmd('iperf -c %s -p %s -n %d -i 1 -yc > /dev/null &' % (server.IP(), port, data_size))
+                
     info('[*] Finalizando o Mininet \n')
     net.stop()
